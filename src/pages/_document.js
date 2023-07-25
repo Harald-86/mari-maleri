b12a6278519fa41334c1cc4b1119f5be209c4f6b
Harald-86 <79138134+Harald-86@users.noreply.github.com>
2023-07-25 11:08:11 +0200

    Malerier
    Oppdatert styling på malerier (selve bildene) Skal være responsive nå

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ import Navbar from "@/components/nav/navbar";
 export default function Document() {
   return (
     <Html lang="en">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
       <Head />
       <body>
         <div className="wrapper">
